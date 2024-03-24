68ff3f66794bb300d5280712165bde0251234b73
Fernanda Leite <edinaldofpinheiro@gmail.com>
2024-03-24 19:15:47 -0300

    primeiro commit da responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import iconLuxury from "./assets/icon-luxury.svg"
 
 export default function App() {
   return (
-    <main className="min-h-svh b-offwhitebg py-20 px-6">
-      <section>
+    <main className="min-h-svh b-offwhitebg py-20 px-6 antialiased lg:flex justify-center items-center">
+      <section className="lg:flex">
         <div className="bg-orange p-12 rounded-t-lg">
           <img src={iconSedans} alt="Ícone dos carros sedan" className="mb-7"/>
-          <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Sedans</h2>
+          <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg ">Sedans</h2>
           <p className="font-lexend mb-7 text-offwhite text-same">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
           <a className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-orange" href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
         </div>
